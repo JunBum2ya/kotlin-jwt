0293e3c4d593ac5aadb401a01aaf58b5ecdeb9de
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2023-09-08 21:34:28 +0900

    #init

---
+++ wanted/wanted/src/main/kotlin/com/kotlin/wanted/WantedApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.wanted
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WantedApplication
+
+fun main(args: Array<String>) {
+	runApplication<WantedApplication>(*args)
+}
