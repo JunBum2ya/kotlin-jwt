878447f1731b54f1139f32db861147357ec90adc
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2023-11-14 21:35:20 +0900

    thymeleaf 기능 추가
    템플릿 추가

--- wanted/wanted/src/main/kotlin/com/kotlin/wanted/global/config/ThymeleafConfig.kt
+++ wanted/wanted/src/main/kotlin/com/kotlin/wanted/global/config/ThymeleafConfig.kt
@@ -1,7 +1,18 @@
 package com.kotlin.wanted.global.config
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver
 
 @Configuration
 class ThymeleafConfig {
+    @Bean
+    fun thymeleafTemplateResolver(defaultTemplateResolver: SpringResourceTemplateResolver,thymeleaf3Properties: Thymeleaf3Properties) : SpringResourceTemplateResolver {
+        defaultTemplateResolver.useDecoupledLogic = thymeleaf3Properties.decoupledLogic
+        return defaultTemplateResolver
+    }
 }
+@ConfigurationProperties(value = "spring.thymeleaf3")
+class Thymeleaf3Properties(val decoupledLogic : Boolean = false)
+
